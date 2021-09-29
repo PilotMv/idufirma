08794bcb80aa59c31fbd0c2adcc29137e0564078
Goldenbeasty <73758693+Goldenbeasty@users.noreply.github.com>
2021-09-29 23:27:16 +0300

    Cleanup

--- code.py
+++ code.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 
-
-
 import Adafruit_DHT
 import requests
 import time
@@ -18,22 +16,14 @@ sensorpin1 = 17
 def sample():
     humsealing, tempsealing = Adafruit_DHT.read_retry(sensormodel, sensorpin)
 
-
-
     humout, tempout = Adafruit_DHT.read_retry(sensormodel, sensorpin1)
-
-
 
     if humsealing is None or tempsealing is None:
         mf = requests.post('https://api.thingspeak.com/update.json', data = {'api_key':thingspeak_key, 'status':'failed to get reading'})
 
-
     else:
-
         r = requests.post('https://api.thingspeak.com/update?', data = {'api_key':thingspeak_key, 'field1':tempsealing, 'field2':humsealing,'field3':tempout, 'field4':humout})
-    
-   
+
 sample()
 time. sleep(30)
 sample()
-
