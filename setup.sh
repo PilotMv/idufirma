e31e7e91f00299b46e2c57cc5ebdb8936779593c
Goldenbeasty <73758693+Goldenbeasty@users.noreply.github.com>
2021-10-21 22:41:54 +0300

    update me

--- setup.sh
+++ setup.sh
@@ -5,6 +5,9 @@ echo "You must be the superuser to run this script" >&2
 exit 1
 fi
 
+sudo apt update -y
+sudo apt updgrade -y
+
 sudo pip3 install Adafruit_DHT
 sudo echo "
 0,30 * * * *  python3" pwd >> /tmp/cron*/crontab
