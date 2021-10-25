757882ca04995cc0e70ec420094856367de0e804
PilotMv <91613636+PilotMv@users.noreply.github.com>
2021-10-25 15:24:08 +0300

    @Goldenbeasty helped me

--- code.py
+++ code.py
@@ -5,11 +5,11 @@ import Adafruit_DHT # Import a (probably proprietary) deprecated library https:/
 import requests
 import time
 from struct import pack, unpack
+import RPi.GPIO as GPIO
 
-sleeptime = 30 # User configurable part # in seconds
+sleeptime = 5 # User configurable part # in seconds
 samplecount = 3 # Samples per run
 sensorpin = 4
-sensorpin1 = 17
 reportunstable = True
 
 f = open('config.ini', 'r') # Opening config.ini file
@@ -18,71 +18,67 @@ f.close()
 
 sensormodel = Adafruit_DHT.AM2302 # Importing sensor for DHT library
 
-allsum_humsealing = 0.0 # Not sure if needed, just keeping that for safety
-allsum_tempsealing = 0.0
-allsum_humout = 0.0
-allsum_tempout = 0.0
+allsum_hum = 0.0 # Not sure if needed, just keeping that for safety
+allsum_temp = 0.0
 
-tolerance_humout = 40
-tolerance_humsealing = 20
-tolerance_tempout = 10
-tolerance_tempsealing = 5
+tolerance_hum = 20
+tolerance_temp = 5
+
+target_temp = 29
+relay_pin = 11
 
 def sample():
-    global humsealing
-    global tempsealing
-    global humout
-    global tempout
-    humsealing, tempsealing = Adafruit_DHT.read_retry(sensormodel, sensorpin) # Samples sensors and temporarely stores values in a float
-    humout, tempout = Adafruit_DHT.read_retry(sensormodel, sensorpin1)
+    global hum
+    global temp
+    hum, temp = Adafruit_DHT.read_retry(sensormodel, sensorpin) # Samples sensors and temporarely stores values in a float
 
-    if humsealing is None or tempsealing is None:
+    if hum is None or temp is None:
         mf = requests.post('https://api.thingspeak.com/update.json', data = {'api_key':thingspeak_key, 'status':'failed to get reading'}) # In case of a measuring failure sends failed to get reading
 
 for x in range(samplecount):
     sample()
-    allsum_humout = allsum_humout + humout # Adds all values to sums, later used to calculate the avarage, too bored to do it with an array
-    allsum_humsealing = allsum_humsealing + humsealing
-    allsum_tempout = allsum_tempout + tempout
-    allsum_tempsealing = allsum_tempsealing + tempsealing
+    allsum_hum = allsum_hum + hum # Adds all values to sums, later used to calculate the avarage, too bored to do it with an array
+    allsum_temp = allsum_temp + temp
     if (x != samplecount): # In case of the last measurement no delay is needed
         time.sleep(sleeptime)
 
-humout = allsum_humout / samplecount # Calculating avarages
-humsealing = allsum_humsealing / samplecount
-tempout = allsum_tempout / samplecount
-tempsealing = allsum_tempsealing /samplecount
+hum = allsum_hum / samplecount# Calculating avarages
+temp = allsum_temp /samplecount
 try:
     with open('.last', 'rb') as file:
         packed = file.read()
         array = unpack('d' * (len(packed) // 8), packed) # 8 bytes per double
         file.close()
 except FileNotFoundError:
-    array = [0,0,0,0]
+    array = [0,0]
     with open('.last', 'wb') as file:
         file.write(pack('d' * len(array) , *array))
         file.close()
 
-if (abs(array[0] - humout) > tolerance_humout or abs(array[1] - humsealing) > tolerance_humsealing or abs(array[2] - tempout) > tolerance_tempout or abs(array[3] - tempsealing) > tolerance_tempsealing):
+if (abs(array[0] - hum) > tolerance_hum or abs(array[1] - temp) > tolerance_temp):
     if (reportunstable):
         mf = requests.post('https://api.thingspeak.com/update.json', data = {'api_key':thingspeak_key, 'status':'Reading unstable'})
     for x in range(samplecount * 5):
         sample()
-    allsum_humout = allsum_humout + humout # Adds all values to sums, later used to calculate the avarage, too bored to do it with an array
-    allsum_humsealing = allsum_humsealing + humsealing
-    allsum_tempout = allsum_tempout + tempout
-    allsum_tempsealing = allsum_tempsealing + tempsealing
+    allsum_hum = allsum_hum + hum # Adds all values to sums, later used to calculate the avarage, too bored to do it with an array
+    allsum_temp = allsum_temp + temp
     if (x != (samplecount * 5)): # In case of the last measurement no delay is needed
         time.sleep(sleeptime / 10)
 
-    humout = allsum_humout / (samplecount * 5) # Calculating avarages
-    humsealing = allsum_humsealing / (samplecount * 5)
-    tempout = allsum_tempout / (samplecount * 5)
-    tempsealing = allsum_tempsealing /(samplecount * 5)
+    hum = allsum_hum / (samplecount * 5)  # Calculating avarages
+    temp = allsum_temp /(samplecount * 5)
 
-array = [humout, humsealing, tempout, tempsealing] # Store value
+array = [hum, temp] # Store value
 with open('.last', 'wb') as file:
     file.write(pack('d' * len(array) , *array))
     file.close()
 
-r = requests.post('https://api.thingspeak.com/update?', data = {'api_key':thingspeak_key, 'field1':tempsealing, 'field2':humsealing,'field3':tempout, 'field4':humout}) # Finally sending the data
+r = requests.post('https://api.thingspeak.com/update?', data = {'api_key':thingspeak_key, 'field1':temp, 'field2':hum}) # Finally sending the data
+
+GPIO.setmode(GPIO.board)
+GPIO.setup(relay_pin, GPIO.OUT)
+
+if (temp < target_temp):
+    GPIO.output(relay_pin, GPIO.LOW)
+elif (temp > target_temp):
+    GPIO.output(relay_pin, GPIO.HIGH)
