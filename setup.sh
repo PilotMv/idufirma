463912c741be9539bd51b3c70079b5953cdfa15b
PilotMv <91613636+PilotMv@users.noreply.github.com>
2021-10-25 14:02:17 +0300

    @Goldenbeasty thx for help

--- setup.sh
+++ setup.sh
@@ -5,6 +5,9 @@ echo "You must be the superuser to run this script" >&2
 exit 1
 fi
 
+sudo apt-get update
+sudo apt-get install build-essential python3-dev
+sudo apt-get install python3-gpiozero
 sudo pip3 install Adafruit_DHT
 
 #going to also set up the automation
