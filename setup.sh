16eafcc4f5b1f5f43e4775060b55362ae8cb55ec
Goldenbeasty <73758693+Goldenbeasty@users.noreply.github.com>
2021-10-26 19:22:12 +0300

    actually works

--- setup.sh
+++ setup.sh
@@ -9,5 +9,5 @@ sudo apt update -y
 sudo apt updgrade -y
 
 sudo pip3 install Adafruit_DHT
-sudo echo "
-0,30 * * * *  python3" pwd >> /tmp/crontab*/crontab
+BASEDIR=$(pwd $0)
+(crontab -l ; echo "0,30 * * * * python3 ${BASEDIR}/code.py")| crontab -
