f9e20e76ed08831bb069de7bf64203a2802d9979
Goldenbeasty <goldenbeaster@gmail.com>
2022-02-28 00:01:50 +0200

    Redo of code

--- code.py
+++ code.py
@@ -2,54 +2,32 @@
 
 import Adafruit_DHT # https://github.com/adafruit/Adafruit_Python_DHT
 import requests
-import time
-
-sleeptime = 30 # User configurable part # in seconds
-samplecount = 3 # Samples per run
-sensorpin = 4
-sensorpin1 = 17
 
 sensormodel = Adafruit_DHT.AM2302 # Importing sensor for DHT library
+
+# User defined variables
+communicationpins = [7, 15, 16, 20, 30]
+temp_field_names = ['temp1', 'temp2', 'temp3', 'temp4', 'temp5']
+hum_field_names = ['hum1', 'hum2', 'hum3', 'hum4', 'hum5']
 
 with open('config.ini', 'r') as file:
         global thingspeak_key 
         thingspeak_key = file.read()
         file.close()
 
-def sample():
-    global humsealing
-    global tempsealing
-    global humout
-    global tempout
-    humsealing, tempsealing = Adafruit_DHT.read_retry(sensormodel, sensorpin) # Samples sensors and temporarely stores values in a float
-    humout, tempout = Adafruit_DHT.read_retry(sensormodel, sensorpin1)
-
-    if humsealing is None or tempsealing is None:
-        requests.post('https://api.thingspeak.com/update.json', data = {'api_key':thingspeak_key, 'status':'failed to get reading'}) # In case of a measuring failure sends failed to get reading
-
-
+def get_sensor(compin):
+    hum, temp = Adafruit_DHT.read_retry(sensormodel, compin)
+    return temp, hum
 
 def main():
-    # allsum_humsealing = 0.0 # Not sure if needed, just keeping that for safety
-    # allsum_tempsealing = 0.0
-    # allsum_humout = 0.0
-    # allsum_tempout = 0.0
+    data = {'api_key':thingspeak_key}
 
-    for x in range(samplecount):
-        sample()
-        allsum_humout = allsum_humout + humout # Adds all values to sums, later used to calculate the avarage, too bored to do it with an array
-        allsum_humsealing = allsum_humsealing + humsealing
-        allsum_tempout = allsum_tempout + tempout
-        allsum_tempsealing = allsum_tempsealing + tempsealing
-        if (x != samplecount): # In case of the last measurement no delay is needed
-            time.sleep(sleeptime)
+    for i in range(len(communicationpins)):
+        temp, hum = get_sensor(communicationpins[i])
+        data[temp_field_names[i]] = temp
+        data[hum_field_names[i]] = hum
 
-    humout = allsum_humout / samplecount # Calculating avarages
-    humsealing = allsum_humsealing / samplecount
-    tempout = allsum_tempout / samplecount
-    tempsealing = allsum_tempsealing /samplecount
-
-    requests.post('https://api.thingspeak.com/update?', data = {'api_key':thingspeak_key, 'field1':tempsealing, 'field2':humsealing,'field3':tempout, 'field4':humout}) # Finally sending the data
+    requests.post('https://api.thingspeak.com/update?', data)
 
 if __name__ == '__main__':
     main()
