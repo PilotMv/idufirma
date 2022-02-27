806683c013a65370f093d9c94cb9ebb620fe2f57
Goldenbeasty <goldenbeaster@gmail.com>
2022-02-28 00:05:18 +0200

    user key reviewers

--- code.py
+++ code.py
@@ -10,17 +10,18 @@ communicationpins = [7, 15, 16, 20, 30]
 temp_field_names = ['temp1', 'temp2', 'temp3', 'temp4', 'temp5']
 hum_field_names = ['hum1', 'hum2', 'hum3', 'hum4', 'hum5']
 
-with open('config.ini', 'r') as file:
-        global thingspeak_key 
-        thingspeak_key = file.read()
-        file.close()
+def get_user_key():
+    with open('config.ini', 'r') as file:
+            thingspeak_key = file.read()
+            file.close()
+    return thingspeak_key
 
 def get_sensor(compin):
     hum, temp = Adafruit_DHT.read_retry(sensormodel, compin)
     return temp, hum
 
 def main():
-    data = {'api_key':thingspeak_key}
+    data = {'api_key' : get_user_key()}
 
     for i in range(len(communicationpins)):
         temp, hum = get_sensor(communicationpins[i])
